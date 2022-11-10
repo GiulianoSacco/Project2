e693413f3f251d68f2177f76e99e2127be4e4c8d
Giuliano Sacco <saccogiuliano99@gmail.com>
2022-11-10 17:18:46 +0100

    work

--- Project/routes/activity.routes.js
+++ Project/routes/activity.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+
 const Activity = require("../models/Activity.model")
 const User = require("../models/User.model")
+const fileUploader = require('../config/cloudinary.config');
 const isLoggedIn = require("../middleware/isLoggedIn");
 const isLoggedOut = require("../middleware/isLoggedOut");
 
@@ -15,12 +17,12 @@ router.get("/create", isLoggedIn, (req, res) => {
 
 
 
-router.post("/create", async (req, res) => {
+router.post("/create", fileUploader.single('activity-image'), async (req, res) => {
   const {activity, description, find, from, to, locations} = req.body
   console.log(req.body)
   const author = req.session.currentUser._id
   try{
-      const newActivity = await Activity.create({author, activity, description, find, from, to, locations})
+      const newActivity = await Activity.create({author, activity, description, find, from, to, locations, image: req.file.path})
       const userUpdated = await User.findByIdAndUpdate(author, { $push: { activityIds: newActivity._id } })
       res.redirect("/")
   }catch (err){
@@ -164,38 +166,7 @@ router.post('/description/:activityId/delete', async (req, res) => {
     
 });
 
-router.get("/map", (req, res) => {
-  // Define the locations of the map with the info we want to display in the popup Paso 7
-  const locations = [{
-    id: 1,
-    name: "Barcelona",
-    pos: [2.154007, 41.390205],
-    img: "https://zfbarcelona.es/wp-content/uploads/2021/01/czfb-web-portada2-768x343.jpg"
-  }, {
-    id: 2,
-    name: "Madrid",
-    pos: [-3.703339, 40.416729],
-    img: "https://www.coldwellbanker.es/wp-content/uploads/2022/04/madrid-2.jpeg"
-  }, {
-    id: 3,
-    name: "Olot",
-    pos: [2.488024, 42.182208],
-    img: "https://es.turismegarrotxa.com/img-municipi-3200-1800/volca-montsacopa-dron-2019_001.jpg"
-  },{
-    id: 4,
-    name: "Lisbon",
-    pos: [-9.142685, 38.736946],
-    img: "https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/Portugal/Lisbon/lisbon-lead-image.jpg?imwidth=680"
-  }]
-  
-  // Define the dislay of the map Paso 8
-  const mapCenter = [-3.703339, 40.416729]
-  const mapZoom = 5
 
-  // Render the map Paso 9
-  res.render("activity/activity-description", { locations, mapCenter, mapZoom  })
-
-})
 
 
 
